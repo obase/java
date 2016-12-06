94e19bf429ee8f782223a55191fc0973add68490
jason <jason@JASONPC>
2016-12-06 18:07:02 +0800

    add security defender

---
+++ obase-webc/src/main/java/com/github/obase/webc/SecurityDefenderFilter.java
@@ -0,0 +1,30 @@
+package com.github.obase.webc;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SecurityDefenderFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// nothing to do
+		return;
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
