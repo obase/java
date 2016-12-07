e733e97cfa20179772b9999326dd9def39922faa
jason <jason@JASONPC>
2016-12-07 14:00:28 +0800

    as map

--- obase-util/src/main/java/com/github/obase/kit/MapKit.java
+++ obase-util/src/main/java/com/github/obase/kit/MapKit.java
@@ -1,5 +1,6 @@
 package com.github.obase.kit;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapKit {
@@ -10,6 +11,23 @@ public class MapKit {
 
 	public static boolean isNotEmpty(Map<?, ?> colls) {
 		return colls != null && colls.size() != 0;
+	}
+
+	public static Map<String, Object> as(Object... pairs) {
+		if (pairs == null) {
+			return null;
+		}
+		Map<String, Object> ret = new HashMap<String, Object>((pairs.length + 1) / 2);
+		int k = 0, v = 1;
+		while (v < pairs.length) {
+			ret.put((String) pairs[k], pairs[v]);
+			k += 2;
+			v += 2;
+		}
+		if (k < pairs.length && v == pairs.length) { // last key
+			ret.put((String) pairs[k], null);
+		}
+		return ret;
 	}
 
 }
