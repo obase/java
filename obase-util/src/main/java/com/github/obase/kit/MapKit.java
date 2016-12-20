ff5bc642f57b66910abba968d05747e5ade64006
jason <jason@JASONPC>
2016-12-21 00:20:58 +0800

    MapKit add as2

--- obase-util/src/main/java/com/github/obase/kit/MapKit.java
+++ obase-util/src/main/java/com/github/obase/kit/MapKit.java
@@ -30,4 +30,21 @@ public class MapKit {
 		return ret;
 	}
 
+	public static Map<String, String> as2(String... pairs) {
+		if (pairs == null) {
+			return null;
+		}
+		Map<String, String> ret = new HashMap<String, String>((pairs.length + 1) / 2);
+		int k = 0, v = 1;
+		while (v < pairs.length) {
+			ret.put((String) pairs[k], pairs[v]);
+			k += 2;
+			v += 2;
+		}
+		if (k < pairs.length && v == pairs.length) { // last key
+			ret.put((String) pairs[k], null);
+		}
+		return ret;
+	}
+
 }
