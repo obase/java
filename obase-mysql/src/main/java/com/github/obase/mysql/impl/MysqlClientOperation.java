cc4619c102c7e827903efe97bf2a7b30f174c3b6
jasonhe.hzw@foxmail.com <jasonhe.hzw@foxmail.com>
2017-02-19 01:32:55 +0800

    ready to test queryXXXExtc

--- obase-mysql/src/main/java/com/github/obase/mysql/impl/MysqlClientOperation.java
+++ obase-mysql/src/main/java/com/github/obase/mysql/impl/MysqlClientOperation.java
@@ -21,7 +21,9 @@ import java.sql.SQLXML;
 import java.sql.Statement;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -41,6 +43,7 @@ import com.github.obase.MessageException;
 import com.github.obase.Page;
 import com.github.obase.WrappedException;
 import com.github.obase.kit.ClassKit;
+import com.github.obase.kit.MapKit;
 import com.github.obase.mysql.asm.AsmKit;
 import com.github.obase.mysql.config.ConfigMetaInfo;
 import com.github.obase.mysql.config.ConfigSAXParser;
@@ -1641,4 +1644,342 @@ abstract class MysqlClientOperation {
 		return action;
 	}
 
+	/*
+	 * special config query to process collection parameters
+	 */
+
+	private static String newParamName(Object name, int idx) {
+		return new StringBuilder(64).append(name).append("_$").append(idx).toString();
+	}
+
+	/** scan to recognize collection params and convert it. return collection params */
+	private static Map<String, Object> extcCollectValues(Map<String, Object> values, Map<String, Integer> collects) {
+
+		Map<String, Object> valuesNew = new HashMap<String, Object>();
+		for (Map.Entry<String, Object> entry : values.entrySet()) {
+			String key = entry.getKey();
+			Object val = entry.getValue();
+			if (val instanceof Collection) {
+				Collection<?> c = (Collection<?>) val;
+				int size = c.size();
+				if (size == 1) {
+					valuesNew.put(key, c.iterator().next());
+				} else if (size == 0) {
+					valuesNew.put(key, null);
+				} else {
+					int idx = 0;
+					for (Iterator<?> iter = c.iterator(); iter.hasNext(); idx++) {
+						String nameNew = newParamName(iter.next(), idx);
+						valuesNew.put(nameNew, iter.next());
+					}
+					collects.put(key, size);
+				}
+			} else {
+				valuesNew.put(key, val);
+			}
+		}
+		return valuesNew;
+	}
+
+	/** Position params, start from 1 */
+	private static String[] origSqlMetaParams(SqlMeta meta) {
+		if (MapKit.isEmpty(meta.params)) {
+			return null;
+		}
+		int size = 1;
+		for (int[] arr : meta.params.values()) {
+			size += arr.length;
+		}
+		String[] params = new String[size];
+		for (Map.Entry<String, int[]> entry : meta.params.entrySet()) {
+			String key = entry.getKey();
+			for (int pos : entry.getValue()) {
+				params[pos] = key;
+			}
+		}
+		return params;
+	}
+
+	/** syntax:((...?..)) */
+	private static void extcCollectPsql(StringBuilder sqlb, int size, int idx, int lbound, int rbound) throws SQLException {
+		int pidx = sqlb.lastIndexOf("((", idx);
+		if (pidx < 0 || pidx <= lbound) {
+			throw new SQLException("Syntax error for param near index " + idx + ": " + sqlb);
+		}
+		int sidx = sqlb.indexOf("))", idx);
+		if (sidx < 0 || sidx >= rbound) {
+			throw new SQLException("Syntax error for param near index " + idx + ": " + sqlb);
+		}
+		pidx += 1;
+		sidx += 1;
+		String str = " OR " + sqlb.substring(pidx, sidx);
+		for (int i = 1; i < size; i++) {
+			sqlb.insert(sidx, str);
+		}
+	}
+
+	private static int[] toIntArray(List<Integer> list) {
+		int[] ret = new int[list.size()];
+		int idx = 0;
+		for (Integer p : list) {
+			ret[idx++] = p.intValue();
+		}
+		return ret;
+	}
+
+	private static Map<String, int[]> extcCollectParams(List<String> params) {
+
+		Map<String, List<Integer>> temp = new HashMap<String, List<Integer>>();
+		int pos = 0;
+		for (String param : params) {
+			List<Integer> poses = temp.get(param);
+			if (poses == null) {
+				poses = new LinkedList<Integer>();
+			}
+			poses.add(++pos);
+		}
+
+		Map<String, int[]> extcParams = new HashMap<String, int[]>(temp.size());
+		for (Map.Entry<String, List<Integer>> entry : temp.entrySet()) {
+			extcParams.put(entry.getKey(), toIntArray(entry.getValue()));
+		}
+		return extcParams;
+	}
+
+	private static SqlMeta extcCollectSqlMeta(SqlMeta meta, Map<String, Integer> collects, boolean limitIndexed) throws SQLException {
+
+		String[] origParams = origSqlMetaParams(meta); // position start from 1
+
+		LinkedList<String> tempParams = new LinkedList<String>();
+		StringBuilder tempPsql = new StringBuilder(meta.psql);
+		String param;
+		Integer size;
+		for (int limit = meta.psql.length(), last = origParams.length - 1, pos = last; pos > 0; pos--) {
+			param = origParams[pos];
+			size = collects.get(param);
+			if (size != null) {
+				for (int idx = size - 1; idx >= 0; idx--) {
+					tempParams.addFirst(newParamName(param, idx));
+				}
+				extcCollectPsql(tempPsql, size, meta.psqlParamIndex[pos], pos > 1 ? meta.psqlParamIndex[pos - 1] : -1, pos < last ? meta.psqlParamIndex[pos + 1] : limit);
+			} else {
+				tempParams.addFirst(param);
+			}
+		}
+		// optional: limitIndex
+		return new SqlMeta(tempPsql.toString(), extcCollectParams(tempParams), limitIndexed ? SqlMetaKit.parseLimitIndexIfExist(tempPsql) : -1);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> List<T> queryExtc(Connection conn, String queryId, Class<T> elemType, Map<String, Object> values) throws SQLException {
+
+		SqlMeta meta = configSqlMetaCache.get(queryId);
+		if (meta == null) {
+			throw new MessageException(MysqlErrno.SOURCE, MysqlErrno.SQL_CONFIG_NOT_FOUND, "Not found sql config: " + queryId);
+		} else if (showSql) {
+			logger.info("Sql for query: " + meta);
+		}
+
+		Map<String, Integer> collects = new HashMap<String, Integer>();
+		Map<String, Object> extcValues = extcCollectValues(values, collects);
+		if (MapKit.isNotEmpty(collects)) {
+			meta = extcCollectSqlMeta(meta, collects, false);
+		}
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(meta.psql);
+			if (extcValues != null) {
+				JdbcAction setAction = JdbcAction.MAP_JDBC_ACTION;
+				setAction.setParam(pstmt, meta.params, extcValues);
+			}
+			rs = pstmt.executeQuery();
+			if (meta.labels == null) {
+				SqlMetaKit.fillSqlMetaLables(rs, meta);
+			}
+			List<T> list = new LinkedList<T>();
+			JdbcAction getAction = getJdbcAction(elemType);
+			while (rs.next()) {
+				list.add((T) getAction.getResult(rs, meta.labels));
+			}
+			return list;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> List<T> queryRangeExtc(Connection conn, String queryId, Class<T> elemType, int start, int max, Map<String, Object> values) throws SQLException {
+
+		SqlMeta meta = configSqlMetaCache.get(queryId);
+		if (meta == null) {
+			throw new MessageException(MysqlErrno.SOURCE, MysqlErrno.SQL_CONFIG_NOT_FOUND, "Not found sql config: " + queryId);
+		}
+
+		String psql = SqlMetaKit.modifyPsqlForLimit(meta, start, max);
+		if (showSql) {
+			logger.info("Sql for queryRange: " + meta.toString(psql));
+		}
+
+		Map<String, Integer> collects = new HashMap<String, Integer>();
+		Map<String, Object> extcValues = extcCollectValues(values, collects);
+		if (MapKit.isNotEmpty(collects)) {
+			meta = extcCollectSqlMeta(meta, collects, true);
+		}
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(psql);
+			if (extcValues != null) {
+				JdbcAction setAction = JdbcAction.MAP_JDBC_ACTION;
+				setAction.setParam(pstmt, meta.params, extcValues);
+			}
+			rs = pstmt.executeQuery();
+			if (meta.labels == null) {
+				SqlMetaKit.fillSqlMetaLables(rs, meta);
+			}
+			List<T> list = new LinkedList<T>();
+			JdbcAction getAction = getJdbcAction(elemType);
+			while (rs.next()) {
+				list.add((T) getAction.getResult(rs, meta.labels));
+			}
+			return list;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> T queryFirstExtc(Connection conn, String queryId, Class<T> elemType, Map<String, Object> values) throws SQLException {
+
+		SqlMeta meta = configSqlMetaCache.get(queryId);
+		if (meta == null) {
+			throw new MessageException(MysqlErrno.SOURCE, MysqlErrno.SQL_CONFIG_NOT_FOUND, "Not found sql config: " + queryId);
+		} else if (showSql) {
+			logger.info("Sql for queryFirst: " + meta);
+		}
+
+		Map<String, Integer> collects = new HashMap<String, Integer>();
+		Map<String, Object> extcValues = extcCollectValues(values, collects);
+		if (MapKit.isNotEmpty(collects)) {
+			meta = extcCollectSqlMeta(meta, collects, true);
+		}
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(meta.psql);
+			if (extcValues != null) {
+				JdbcAction setAction = JdbcAction.MAP_JDBC_ACTION;
+				setAction.setParam(pstmt, meta.params, extcValues);
+			}
+			rs = pstmt.executeQuery();
+			if (meta.labels == null) {
+				SqlMetaKit.fillSqlMetaLables(rs, meta);
+			}
+			JdbcAction getAction = getJdbcAction(elemType);
+			if (rs.next()) {
+				return (T) getAction.getResult(rs, meta.labels);
+			}
+			return null;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> void queryPageExtc(Connection conn, String queryId, Class<T> elemType, Page<T> page, Map<String, Object> values) throws SQLException {
+
+		int offset = page.start;
+		if (offset < 0) {
+			offset = 0;
+		}
+		int count = page.limit;
+		if (count <= 0) {
+			count = Integer.MAX_VALUE;
+		}
+
+		SqlMeta meta = configSqlMetaCache.get(queryId);
+		if (meta == null) {
+			throw new MessageException(MysqlErrno.SOURCE, MysqlErrno.SQL_CONFIG_NOT_FOUND, "Not found sql config: " + queryId);
+		}
+
+		String psql = SqlMetaKit.modifyPsqlForPageLimit(meta, offset, count, page.field, DIRECTION_DESC.equals(page.direction));
+		if (showSql) {
+			logger.info("Sql for queryPage: " + meta.toString(psql));
+		}
+
+		Map<String, Integer> collects = new HashMap<String, Integer>();
+		Map<String, Object> extcValues = extcCollectValues(values, collects);
+		if (MapKit.isNotEmpty(collects)) {
+			meta = extcCollectSqlMeta(meta, collects, true);
+		}
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		JdbcAction setAction = JdbcAction.MAP_JDBC_ACTION;
+
+		try {
+			pstmt = conn.prepareStatement(psql);
+			if (extcValues != null) {
+				setAction.setParam(pstmt, meta.params, extcValues);
+			}
+			rs = pstmt.executeQuery();
+			if (meta.labels == null) {
+				SqlMetaKit.fillSqlMetaLables(rs, meta);
+			}
+			JdbcAction getAction = getJdbcAction(elemType);
+			List<T> data = new LinkedList<T>();
+			while (rs.next()) {
+				data.add((T) getAction.getResult(rs, meta.labels));
+			}
+			page.setRows(data);
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
+
+		if ((offset == 0 && count == Integer.MAX_VALUE) || (page.getRows().size() < count)) {
+			page.setResults(page.getRows().size() + offset);
+		} else {
+			try {
+				pstmt = conn.prepareStatement(SqlMetaKit.modifyPsqlForPageTotal(meta));
+				if (extcValues != null) {
+					setAction.setParam(pstmt, meta.params, extcValues);
+				}
+				rs = pstmt.executeQuery();
+				if (rs.next()) {
+					page.setResults(rs.getInt(1));
+				}
+			} finally {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			}
+		}
+	}
 }
