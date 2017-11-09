2048c019bb03d4d6ff77cb538608a46b78265809
jasonhe.hzw@foxmail.com <jasonhe.hzw@foxmail.com>
2017-11-09 19:09:34 +0800

    add hmac support

---
+++ obase-util/src/main/java/com/github/obase/hash/Hmac.java
@@ -0,0 +1,49 @@
+package com.github.obase.hash;
+
+import java.nio.charset.Charset;
+import java.security.GeneralSecurityException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.github.obase.coding.Hex;
+
+public class Hmac {
+	static final Charset UTF8 = Charset.forName("utf8");
+
+	static final String HmacMD5 = "HmacMD5";
+
+	public static String hmacMD5Hex(String secret, String... args) throws GeneralSecurityException {
+		SecretKeySpec sks = new SecretKeySpec(secret.getBytes(UTF8), HmacMD5);
+		Mac mac = Mac.getInstance(HmacMD5);
+		mac.init(sks);
+		for (String arg : args) {
+			mac.update(arg.getBytes(UTF8));
+		}
+		return Hex.encode(mac.doFinal());
+	}
+
+	static final String HmacSHA128 = "HmacSHA128";
+
+	public static String hmacSHA128Hex(String secret, String... args) throws GeneralSecurityException {
+		SecretKeySpec sks = new SecretKeySpec(secret.getBytes(UTF8), HmacSHA128);
+		Mac mac = Mac.getInstance(HmacSHA128);
+		mac.init(sks);
+		for (String arg : args) {
+			mac.update(arg.getBytes(UTF8));
+		}
+		return Hex.encode(mac.doFinal());
+	}
+
+	static final String HmacSHA256 = "HmacSHA256";
+
+	public static String hmacSHA256Hex(String secret, String... args) throws GeneralSecurityException {
+		SecretKeySpec sks = new SecretKeySpec(secret.getBytes(UTF8), HmacSHA256);
+		Mac mac = Mac.getInstance(HmacSHA256);
+		mac.init(sks);
+		for (String arg : args) {
+			mac.update(arg.getBytes(UTF8));
+		}
+		return Hex.encode(mac.doFinal());
+	}
+}
