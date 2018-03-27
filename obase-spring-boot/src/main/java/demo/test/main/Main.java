4788fb8360f3c11ec2320a65171c789a5ba2589a
jasonhe.hzw@foxmail.com <jasonhe.hzw@foxmail.com>
2018-03-27 11:35:14 +0800

    增加spring-boot的demo

---
+++ obase-spring-boot/src/main/java/demo/test/main/Main.java
@@ -0,0 +1,16 @@
+package demo.test.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import com.github.obase.webc.WebcServletContainerInitializer;
+
+@SpringBootApplication
+public class Main extends WebcServletContainerInitializer implements ServletContextInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMain.class, args);
+	}
+
+}
